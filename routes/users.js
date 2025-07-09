041554b3797a5fd9958ea69c5743c99e0a986df8
rudraksh974 <145356796+rudraksh974@users.noreply.github.com>
2025-07-09 14:04:27 +0530

    update 09-07-2025 2

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const router = express.Router();
+
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.get('/profile', auth, getUserProfile);
+router.get('/', (req, res) => {
+    res.json({ message: 'User API working ✅' });
+  });
+  
 
 module.exports = router;
