303bed35779fa90a4efacb50135792dfc7d15033
rudraksh974 <145356796+rudraksh974@users.noreply.github.com>
2025-07-17 23:21:43 +0530

    update 17-07-2025 2

--- routes/users.js
+++ routes/users.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const { registerUser, loginUser, getUserProfile } = require('../controllers/userController');
 const auth = require('../middleware/auth');
 
-router.post('/register', registerUser);
+router.post('/register', (req, res, next) => {
+  console.log("Register route triggered");
+  next();
+}, registerUser);
 router.post('/login', loginUser);
 router.get('/profile', auth, getUserProfile);
 router.get('/', (req, res) => {
